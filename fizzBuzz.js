fbeae2c7bcab4cf3274569d0eeb0c3b6a2e6bf28
Ben Corson <corsonbenjamin@gmail.com>
2022-06-16 11:29:12 -0500

    added comments

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,11 @@
 // Write your solution below this line:
 
-
-
 for(let x = 1; x < 51; x++){
-    if((x%3)===0 && (x%5)===0){
+    if((x%3)===0 && (x%5)===0){ //divisible by 3 and 5
         console.log('fizzbuzz')
-    } else if((x%5)===0){
+    } else if((x%5)===0){ //divisible by 5
         console.log('buzz')
-    } else if((x%3)===0){
+    } else if((x%3)===0){ //divisible by 3
         console.log('fizz')
     } else {
         console.log(x)
